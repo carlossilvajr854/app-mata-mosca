5055cf22c0395bd748c09c097b554ea3d5f78027
Carlos Silva <carlossilvajr854@gmail.com>
2023-03-20 20:58:40 -0300

    implementacoes-js

--- public/js/jogo.js
+++ public/js/jogo.js
@@ -1,19 +1,38 @@
-// Variáveis
-let alturaPalco = 0;
-let larguraPalco = 0;
-let posicaoX = 0;
-let posicaoY = 0;
+// Variáveis de escopo global
+let alturaPalco;
+let larguraPalco;
+let posicaoX;
 
 function ajustaTamanhoPalco() {
     alturaPalco = window.innerHeight;
     larguraPalco = window.innerWidth;
-
-    console.log(larguraPalco, alturaPalco);
 }
 
 ajustaTamanhoPalco();
 
-posicaoX = Math.floor(Math.random() * larguraPalco);
-posicaoY = Math.floor(Math.random() * alturaPalco);
+function tamanhoAleatorio() {
+    return Math.ceil(Math.random() * 3);
+}
 
-console.log(posicaoX, posicaoY);
+function inverterLadoMosca() {
+    return posicaoX < Math.ceil(larguraPalco / 2 - 45) ? " ladoInvertido" : "";
+}
+
+function posicaoRandomica() {
+    posicaoX = Math.floor(Math.random() * larguraPalco) - 90;
+    let posicaoY = Math.floor(Math.random() * alturaPalco) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    inverterLadoMosca();
+
+    let mosca = document.createElement("img");
+    mosca.src = "/public/img/mosca.png";
+    mosca.className = "mosca" + tamanhoAleatorio() + inverterLadoMosca();
+    mosca.style.position = "absolute";
+    mosca.style.left = posicaoX + "px";
+    mosca.style.top = posicaoY + "px";
+
+    document.body.appendChild(mosca);
+}
